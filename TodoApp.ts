339ad00a2226ba91fd9fb0783acf5d3d4587dd8b
AhteshamMalik <161864884+AhteshamMalik@users.noreply.github.com>
2024-04-06 23:09:22 +0500

    Added new features. Made By : Ahtesham

--- TodoApp.ts
+++ TodoApp.ts
@@ -3,21 +3,39 @@
 import inquirer from "inquirer";
 let todo = [];
 let condition = true;
+let addTask = await inquirer.prompt({
+  name: "ask",
+  message: "What you want to Add?",
+  type: "input",
+});
+todo.push(addTask.ask);
+console.log(todo);
 while (condition) {
-  let addTask = await inquirer.prompt([
-    {
+  let Option = await inquirer.prompt({
+    name: "options",
+    message: "Select the Option: ",
+    type: "list",
+    choices: ["Add", "Remove", "Exit"],
+  });
+
+  if (Option.options === "Add") {
+    addTask = await inquirer.prompt({
       name: "ask",
-      message: "What you want to add?",
+      message: "What you want to Add?",
       type: "input",
-    },
-    {
-      name: "askMore",
-      message: "Do you want to add more?",
+    });
+    todo.push(addTask.ask);
+    console.log(todo);
+  } else if (Option.options === "Remove") {
+    todo.pop();
+    console.log(todo);
+  } else {
+    let getexit = await inquirer.prompt({
+      name: "exit",
+      message: "Don't you want to continue? press n :",
       type: "confirm",
-      default: "false",
-    },
-  ]);
-  todo.push(addTask.ask);
-  condition = addTask.askMore;
-  console.log(todo);
+      default: "true",
+    });
+    condition = getexit.exit;
+  }
 }
